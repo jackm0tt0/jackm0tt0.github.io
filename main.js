b770d4d7ad8522e9748d1cea4e6976d6b84b7ec9
jackm0tt0 <102541616+jackm0tt0@users.noreply.github.com>
2023-08-23 19:23:55 -0500

    3d Test for mom

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // setup scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -11,10 +11,21 @@ document.body.appendChild( renderer.domElement );
 // create cube
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material2 = new THREE.MeshBasicMaterial( { color: 0x336633 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+// Load custom 3d Model
+const loader = new GLTFLoader();
+
+loader.load( './public/models/duck.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+    scene.children[1].children[0].material = material2;
+}, undefined, function ( error ) {
+	console.error( error );
+} );
 
 // create renderer
 function animate() {
