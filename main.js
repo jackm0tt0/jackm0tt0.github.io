d88b13cdc5aa9ab283aa16abd1fb72f9eee75575
jackm0tt0 <102541616+jackm0tt0@users.noreply.github.com>
2024-03-13 00:25:12 +0100

    light mode

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { OutlinePass } from 'three/addons/postprocessing/OutlinePass.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { CCDIKSolver } from 'three/addons/animation/CCDIKSolver.js';
 import Stats from 'three/addons/libs/stats.module.js';
+import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
+import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
+import { Line2 } from 'three/addons/lines/Line2.js';
 
-let camera, scene, renderer;
+
+let camera, look, scene, renderer;
 let composer, outlinePass;
 let controls, gui;
 
@@ -21,20 +24,79 @@ let selectedObjects = [];
 let prevTime = performance.now();
 
 
+//determines if the user has a set theme
+function detectColorScheme(){
+    var theme="light";    //default to light
+
+    //local storage is used to override OS theme settings
+    if(localStorage.getItem("theme")){
+        if(localStorage.getItem("theme") == "dark"){
+            var theme = "dark";
+        }
+    } else if(!window.matchMedia) {
+        //matchMedia method not supported
+        return false;
+    } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        //OS theme setting detected as dark
+        var theme = "dark";
+    }
+
+    //dark theme preferred, set document with a `data-theme` attribute
+    if (theme=="dark") {
+         document.documentElement.setAttribute("data-theme", "dark");
+    }
+}
+detectColorScheme();
+
+//identify the toggle switch HTML element
+// const toggleSwitch = document.getElementById( 'checkbox-theme' );
+const toggleSwitch = document.getElementsByClassName( 'checkbox-theme')[0];
+
+//function that changes the theme, and sets a localStorage variable to track the theme between page loads
+function switchTheme(e) {
+    if (e.target.checked) {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'dark');
+        toggleSwitch.checked = true;
+    } else {
+        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'light');
+        toggleSwitch.checked = false;
+    }    
+}
+
+//listener for changing themes
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+//pre-check the dark-theme checkbox if dark-theme is set
+if (document.documentElement.getAttribute("data-theme") == "dark"){
+    toggleSwitch.checked = true;
+}
+
+
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
+
 const default_robot_pos = new THREE.Vector3(100,10,350);
+const default_camera_position = new THREE.Vector3(4000,-4000,3400);
+const default_look = new THREE.Vector3(0,0,0);
+
 const mouse_3d = default_robot_pos.clone();
 const target = new THREE.Object3D();
 target.position.set(mouse_3d);
 
-const scene_width = 1000;
+const scene_width = 1100;
+
+let matLine;
 
 const stats = new Stats();
 const statsContainer = document.getElementById( 'stats' );
-stats.domElement.style.position = 'absolute';
+//stats.domElement.style.position = 'absolute';
 stats.domElement.style.top = '0px';
 statsContainer.appendChild( stats.domElement );
+
+const RenderSpacer = document.getElementById( 'spacer' );
+
 
 init();
 animate();
@@ -53,11 +115,10 @@ function init() {
     canvasContainer.appendChild(renderer.domElement);
 
     // init camera
-    camera = new THREE.OrthographicCamera(-1,1,1,-1,0.01,10000);
-    resize();
+    camera = new THREE.OrthographicCamera(-1,1,1,-1,0.01,100000);
 
-    camera.position.set( 1400, -1400, 1200 );
-    let look = new THREE.Vector3(0,0,200)
+    camera.position.copy(default_camera_position);
+    look = new THREE.Vector3().copy(default_look);
     camera.lookAt(look);
     
     // init scene
@@ -70,6 +131,18 @@ function init() {
 
     const domeLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
     scene.add(domeLight);
+
+    // Create a material for lines
+    matLine = new LineMaterial( {
+
+        color: 0x00ff00,
+        linewidth: 4, // in pixels
+        //resolution:  // to be set by renderer, eventually
+        dashed: false,
+        alphaToCoverage: true,
+        vertexColors: true,
+    
+    } );
     
     // Load from Rhino
     const rh_loader = new Rhino3dmLoader();
@@ -85,15 +158,40 @@ function init() {
         robot_parts.joints = {};
         rh_object.traverse( n => {
 
+            // General settings
+
+            // if (n instanceof THREE.Mesh){
+            //     n.receiveShadow = true;
+            //     n.castShadow = true;
+            // }
+
+            // Collect Robot Parts
+
             if (n.name === "rob_base") robot_parts.base = n;
-            if (n.name === "rob_0") robot_parts.joints.j0 = n;
-            if (n.name === "rob_1") robot_parts.joints.j1 = n;
-            if (n.name === "rob_2") robot_parts.joints.j2 = n;
-            if (n.name === "rob_3") robot_parts.joints.j3 = n;
-            if (n.name === "rob_4") robot_parts.joints.j4 = n;
-            if (n.name === "rob_5") robot_parts.joints.j5 = n;
-            if (n.name === "rob_skeleton") robot_parts.skeleton_polyline = n;
-            if (n.name === "rob_zone") robot_parts.zone = n;
+            else if (n.name === "rob_0") robot_parts.joints.j0 = n;
+            else if (n.name === "rob_1") robot_parts.joints.j1 = n;
+            else if (n.name === "rob_2") robot_parts.joints.j2 = n;
+            else if (n.name === "rob_3") robot_parts.joints.j3 = n;
+            else if (n.name === "rob_4") robot_parts.joints.j4 = n;
+            else if (n.name === "rob_5") robot_parts.joints.j5 = n;
+            else if (n.name === "rob_skeleton") robot_parts.skeleton_polyline = n;
+            else if (n.name === "rob_zone") robot_parts.zone = n;
+
+            if (n.type === "Line"){
+
+                let geom = new LineGeometry();
+                geom.setPositions( n.geometry.attributes.position.array );
+                
+                let newline = new Line2(geom, matLine);
+                newline.position.set(0,0,0);
+                //newline.boundingSphere = new THREE.Sphere(newline.position, 1);
+                newline.computeLineDistances();
+                newline.scale.set(1,1,1);
+                scene.add(newline);
+                n.visible = false;
+            }
+            
+
         });
 
         // setup robot
@@ -104,6 +202,13 @@ function init() {
 
         // hide loading sequence
         document.getElementById( 'loader' ).style.display = 'none';
+        
+        resize();
+
+        // Event Subscriptions
+        window.addEventListener( 'resize', resize );
+        window.addEventListener( 'scroll' , scroll);
+        window.addEventListener( "mousemove" , mouseMove);
 
     }, function ( progress ) {
 
@@ -126,76 +231,88 @@ function init() {
     composer.addPass( renderPass );
 
     // outlines for interactive objects
+    console.log(renderer);
     outlinePass = new OutlinePass( new THREE.Vector2( window.innerWidth, window.innerHeight ), scene, camera );
+    console.log(outlinePass);
     //composer.addPass( outlinePass );
 
-    renderer.domElement.style.touchAction = 'none';
-    renderer.domElement.addEventListener( 'pointermove', onPointerMove );
+}
 
-    function onPointerMove( event ) {
+function mouseMove( event ) {
 
-        if ( event.isPrimary === false ) return;
+    //if ( event.isPrimary === false ) return;
 
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-        checkIntersection();
+    checkIntersection();
 
+}
+
+// function addSelectedObject( object ) {
+
+//     selectedObjects = [];
+//     selectedObjects.push( object );
+
+// }
+
+function checkIntersection() {
+
+    raycaster.setFromCamera( mouse.clone(), camera );
+
+    const intersects = raycaster.intersectObject( scene, true );
+
+    let robot_hit = raycaster.intersectObjects([robot_zone]);
+
+    if (robot_hit.length > 0 ) {
+        mouse_3d.copy(robot_hit[1].point.clone());
+    }else{
+        mouse_3d.copy(default_robot_pos);
     }
 
-    function addSelectedObject( object ) {
+    // if ( intersects.length > 0 ) {
 
-        selectedObjects = [];
-        selectedObjects.push( object );
+    //     const selectedObject = intersects[ 0 ].object;
+    //     addSelectedObject( selectedObject );
+    //     outlinePass.selectedObjects = selectedObjects;
 
+    // } else {
+        
+    //     //outlinePass.selectedObjects = [];
+
+    // }
+}
+
+function scroll() {
+
+    const scroll_factor = 1275;
+
+    var rect = RenderSpacer.getBoundingClientRect();
+    var centerY = rect.top + rect.height / 2;
+    var delta_y
+
+    if (window.innerWidth < window.innerHeight){
+        delta_y = -(window.innerHeight/2 - centerY) / (window.innerWidth-18);
+    }
+    else {
+        delta_y = -(window.innerHeight/2 - centerY) / window.innerHeight;
     }
 
-    function checkIntersection() {
+    look.z = default_look.z + delta_y * scroll_factor;
+    camera.position.z = default_camera_position.z + delta_y * scroll_factor;
 
-        raycaster.setFromCamera( mouse.clone(), camera );
-
-        const intersects = raycaster.intersectObject( scene, true );
-
-        let robot_hit = raycaster.intersectObjects([robot_zone]);
-
-        if (robot_hit.length > 0 ) {
-            console.log("here")
-            mouse_3d.copy(robot_hit[1].point.clone());
-        }else{
-            mouse_3d.copy(default_robot_pos);
-        }
-
-        if ( intersects.length > 0 ) {
-
-            const selectedObject = intersects[ 0 ].object;
-            addSelectedObject( selectedObject );
-            outlinePass.selectedObjects = selectedObjects;
-
-        } else {
-
-            
-            outlinePass.selectedObjects = [];
-
-        }
-
-    }
-
-    // Event Subscriptions
-    window.addEventListener( 'resize', resize );
-
+    checkIntersection();
 }
 
 function resize() {
 
-    
-
-    const width = window.innerWidth;
+    const width = window.innerWidth -18;
     const height = window.innerHeight;
     camera.aspect = width / height;
 
     let sf = scene_width / height
     
-    if (window.innerWidth < height){
+    if (width < height){
         camera.left = -height/2*sf;
         camera.right = height/2*sf;
         camera.top = height**2 / width / 2*sf; 
@@ -206,29 +323,38 @@ function resize() {
         camera.top = height/ 2*sf; 
         camera.bottom = -height/ 2*sf;
     }
+    
+    scroll();
+
+    // set line resolution.
+    matLine.resolution.set( window.innerWidth, window.innerHeight );
 
     camera.updateProjectionMatrix();
-    renderer.setSize( width, height );
+    renderer.setSize( width, height );   
 
 }
 
 function animate() {
 
-    //controls.update();
-    //renderer.render( scene, camera );
-    stats.update();
-    ikSolver?.update();
+    // get time
     let time = performance.now();
+    stats.update();
+
+    // update camera
+    camera.lookAt(look);
+
+    // update robot
+    ikSolver?.update();
     if (target.position.distanceTo(mouse_3d)>1){
         target.position.add(mouse_3d.clone().sub( target.position ).setLength((time - prevTime)/5));
     }
     
-    prevTime = time;
-    
-    //console.log(target.position)
-
+    //render stuff
     composer.render( scene, camera );
     requestAnimationFrame( animate );
+
+    // set previous time
+    prevTime = time;
 
 }
 
@@ -257,7 +383,7 @@ function initGUI( layers ) {
 
 function initRobot( robot_parts ){
 
-    // Set target object for IK solver
+    // Set position for  target object for IK solver
     target.position.set(0,0,0);
     scene.add(target);
 
@@ -268,8 +394,11 @@ function initRobot( robot_parts ){
 
     // build bones from the skeleton
 
-    const pts = [];
-    for (let i=0 ; i < robot_parts.skeleton_polyline.geometry.attributes.position.count; i++){
+    let bone_count = robot_parts.skeleton_polyline.geometry.attributes.position.count
+
+    // extract points from skeleton
+    var pts = [];
+    for (let i=0 ; i < bone_count; i++){
         pts.push(new THREE.Vector3(
             robot_parts.skeleton_polyline.geometry.attributes.position.array[i * 3],
             robot_parts.skeleton_polyline.geometry.attributes.position.array[i * 3 + 1],
@@ -284,12 +413,12 @@ function initRobot( robot_parts ){
 
     // "bone0"
     let prevBone = new THREE.Bone();
-    prevBone.position.copy(pts[0]);
     rootBone.add( prevBone );
     bones.push( prevBone );
+    robot_parts.joints["j0"].position.sub(pts[0]);
 
-    // "bone1", "bone2", "bone3"
-    for ( let i = 1; i <= 5; i ++ ) {
+    // bone 1 --> bone n
+    for ( let i = 1; i <= bone_count-2; i ++ ) {
         // create new bone with location relative to origin
         const bone = new THREE.Bone();
         bone.position.copy(pts[i].clone().sub(pts[i-1]));
@@ -307,8 +436,11 @@ function initRobot( robot_parts ){
         bones[i].add(robot_parts.joints["j"+String(i-1)]);
     }
 
+    bones[bone_count-1].add(robot_parts.joints["j"+String(bone_count-2)]);
+
     // "target"
     const targetBone = new THREE.Bone();
+    targetBone.position.sub(pts[0]);
     rootBone.add( targetBone );
     bones.push( targetBone );
 
@@ -321,7 +453,7 @@ function initRobot( robot_parts ){
 
     skmesh.add( bones[ 0 ] ); // "root" bone
     skmesh.bind( skeleton );
-    target.attach(targetBone)
+    target.attach(targetBone) // link to mouse attracted target object
     skmesh.boundingSphere = new THREE.Sphere(skmesh.position, 1);
     scene.add(skmesh)
 
@@ -369,12 +501,8 @@ function initRobot( robot_parts ){
     ikSolver = new CCDIKSolver( skmesh, iks );
 
     const helper = ikSolver.createHelper();
-    //scene.add(helper);
+    ///scene.add(helper);
 
     robot_zone = robot_parts.zone;
-
-    // const plane_geo = new THREE.PlaneGeometry(1000,1000);
-    // robot_plane = new THREE.Mesh( plane_geo, new THREE.MeshBasicMaterial());
-    // robot_plane.position.set(0,0,100);
-    // //scene.add(robot_plane);
 }
+
